6c39fca5a2c80f5ccc0107fc9b4cec08c51b762c
dimulka <dorofeev.dmitriy.gennadich@gmail.com>
2020-08-19 02:07:24 +0300

    Refactoring. Moving, sorting, removing

--- src/main/kotlin/ExpressionEnvironment.kt
+++ src/main/kotlin/ExpressionEnvironment.kt
@@ -1,181 +1,6 @@
-data class Value(val textualValue: String) { // получит expr строку
+// TODO тут вообще страшно. Надо изучить
 
-    var resolvedValue = Float.NaN
-
-    /** Разрешает выражение и возвращает его, кэширует вполе resolvedValue */
-    fun resolve(): Float {
-        try {
-            resolvedValue = textualValue.toFloat()
-        } catch (e: NumberFormatException) {
-        }// исключение числового формати
-
-        val tokens = ArrayList<Any>()
-        var index = 0
-        var numStartIndex = -1
-
-        while (index < textualValue.length) {
-            val c = textualValue[index]
-
-            if (c.isWhitespace()) {
-                index++
-                continue
-            }
-            // парсинг значений с плавющей точкой
-            if (c.isDigit() || c == '_') {   // для чисел, положительных и отрицательных
-                if (numStartIndex == -1) {
-                    numStartIndex = index
-                } else if (c == '_') {
-                    throw IllegalArgumentException("Отрицательный операнд внутри числа")
-                }
-            } else if (!c.isDigit() && c != '.') {
-                // перемещаем текущий число в стек
-                if (numStartIndex != -1) {
-                    // с numStartIndex до Index
-                    val num = textualValue.substring(numStartIndex, index).replace("_", "-").toFloat()
-                    tokens.add(num)
-                }
-                numStartIndex = -1
-            }
-            /** смотрим если это скобка*/
-            if (c == '(') {
-                // ищем подстроку в скобках
-                var bracketDepth = 0
-
-                for (seek in index + 1 until textualValue.length) { // поиск конечной закрывающей скобки
-                    if (textualValue[seek] == '(') // если есть вложенные скобки
-                        bracketDepth++
-
-                    if (textualValue[seek] == ')') {
-                        if (bracketDepth == 0) { //проверим что в далее нет вложенных скобок
-                            val bracketContent = textualValue.substring(index + 1, seek)  // +1 к индексу
-                            val subVal = Value(bracketContent)
-                            tokens.add(subVal.resolve()) // пополняем стек
-                            index = seek
-                            bracketDepth = -1
-                            break
-                        } else {
-                            bracketDepth--
-                        }
-                    }
-                }
-                if (bracketDepth != 1) {
-                    throw IllegalArgumentException("Скобки расставлены неверно")
-                }
-            } else if (Operation.isOperator(c)) {
-                tokens.add(Operation.getOperationForChar(c)!!)
-            } else if (!c.isDigit() && c != '_' && c != '.') {
-                println("Нераспознанный символ: $c. Игнорируется")
-            }
-            //////////
-
-            index++
-        }
-
-        if (numStartIndex != -1) {
-
-            val num = textualValue.substring(numStartIndex, textualValue.length).replace("_", "-").toFloat()
-            tokens.add(num)
-        }
-
-        flatten(tokens, Operation.FACTORIAL)
-        flatten(tokens, Operation.MODULE)
-        flatten(tokens, Operation.POWER)
-        flatten(tokens, Operation.ROOT)
-        flatten(tokens, Operation.DIVIDE, Operation.MULTIPLY)
-        flatten(tokens, Operation.PLUS, Operation.MINUS)
-
-        // токен - 1 символ
-        if (tokens.size != 1) {
-            throw IllegalArgumentException("Невозможно полностью разрешить выражение: ${tokens.joinToString(separator = " ")}")
-        }
-        resolvedValue = tokens[0] as Float
-
-        return resolvedValue
-
-    }
-}
-/**
- * Сглаживает токены, указанные в списке, и упрощает их.
- */
-fun flatten(tokens: ArrayList<Any>, vararg ops: Operation) {
-    val tokenSwap = ArrayList<Any>()
-
-    tokenSwap.addAll(tokens)
-
-    while(!ops.none { tokens.contains(it) }) {
-        println(tokens.joinToString(separator = " "))
-
-        for (i in tokens.indices) {
-            var topBreak = false
-            for(o in ops) {
-                if (tokens[i] == o) {
-                    // левый и правый токен должен быть float
-                    if(i == 0 && o.requireLeftValue()) {
-                        throw IllegalArgumentException("Выражение не может начинаться с оператора: $o")
-                    } else if(i == tokens.size - 1 && o.requireRightValue()) {
-                        throw IllegalArgumentException("Выражение не может заканчиваться оператором: $o")
-                    }
-
-                    if(o.requireRightValue() && o.requireLeftValue()) {
-                        val a = tokens[i - 1] as? Float ?: throw IllegalArgumentException("Цепные операторы: $o и ${tokens[i - 1]}")
-                        val b = tokens[i + 1] as? Float ?: throw IllegalArgumentException("Цепные операторы: $o и ${tokens[i + 1]}")
-
-                        val v = o.execute(a, b)
-
-                        val localIndex = findIndex(tokens[i - 1], tokens[i], tokens[i + 1], list = tokenSwap)
-                        for (j in 0..2) {
-                            tokenSwap.removeAt(localIndex)
-                        }
-
-                        tokenSwap.add(localIndex, v)
-                        topBreak = true
-                        break
-                    } else if(o.requireRightValue()) {
-                        val b = tokens[i + 1] as? Float ?: throw IllegalArgumentException("Цепные операторы: $o и ${tokens[i + 1]}")
-
-                        val v = o.execute(0f, b)
-
-                        val localIndex = findIndex(tokens[i], tokens[i + 1], list = tokenSwap)
-                        for (j in 0..1) {
-                            tokenSwap.removeAt(localIndex)
-                        }
-
-                        tokenSwap.add(localIndex, v)
-                        topBreak = true
-                        break
-                    } else if(o.requireLeftValue()) {
-                        val a = tokens[i - 1] as? Float ?: throw IllegalArgumentException("Цепные операторы: $o и ${tokens[i - 1]}")
-
-                        val v = o.execute(a, 0f)
-
-                        val localIndex = findIndex(tokens[i - 1], tokens[i], list = tokenSwap)
-                        for (j in 0..1) {
-                            tokenSwap.removeAt(localIndex)
-                        }
-
-                        tokenSwap.add(localIndex, v)
-                        topBreak = true
-                        break
-                    }
-                }
-            }
-
-            if(topBreak)
-                break
-        }
-
-        tokens.clear()
-        tokens.addAll(tokenSwap)
-    }
-}
-
-/**
- * находит ииндекс в заданном списке ArrayList
- */
-fun findIndex(vararg objs: Any, list: ArrayList<Any>) = list.indices.firstOrNull { i -> list[i] == objs[0] && objs.indices.none { list[i + it] != objs[it] } } ?: -1
-
-const val VALID_VAR_NAMES = "abcdefghijklmnoqprstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" //функция
-
+const val VALID_VAR_NAMES = "abcdefghijklmnoqprstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 /** различные проверки, описыаем переменные и функции */
 class ExpressionEnvironment {
 
@@ -187,7 +12,6 @@ class ExpressionEnvironment {
     fun putFunction(function: Pair<Char, Char>, value: String) = funcs.put(function, value) //функция с функциями..
 
     fun parseAndPrint(exprRaw: String) {
-
         var varname: Char? = null //
         var expr: String = exprRaw.replace("\\s".toRegex(), "") //входная строка для обработки без пробелов и табов
         /** проверка на функцию или переменную */
@@ -314,8 +138,7 @@ class ExpressionEnvironment {
                         bracketsResolved = false
                         break
                     }
-                }
-                else if (c == ')' && i < expr.length - 1) {
+                } else if (c == ')' && i < expr.length - 1) {
                     if ((expr[i + 1].isLetterOrDigit() || expr[i + 1] == '(') && !Operation.isOperator(expr[i + 1])) {
                         expr = expr.replaceRange(i, i + 1, ")*")
                         bracketsResolved = false
@@ -324,7 +147,7 @@ class ExpressionEnvironment {
                 }
             }
 
-            if(bracketsResolved) {
+            if (bracketsResolved) {
                 break
             }
         }
@@ -332,11 +155,10 @@ class ExpressionEnvironment {
         val v = Value(expr) // class Value!!!
         v.resolve()
 
-        if(varname != null) {
+        if (varname != null) {
             print("$varname=")
             putVariable(varname, v.resolvedValue)
         }
-
 
         println(v.resolvedValue)
     }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+// TODO Считай самое главное - надо добавить тесты на все возможные случаи. Будет гораздо удобнее
+// Сейчам почему-то 1+1 выражение он поймёт, а (1+1) нет, говорит, что скобки расставлены не верно, хотя это не так
+
 fun main() {
     println("Определение переменных !x = 3")
     println("Определение функций !f(x) = x^2")
@@ -20,10 +23,85 @@ fun main() {
     }
 }
 
-/*
-!x = 3 - задаём переменную
-!f(x)=3x - функцию
-f(x) - вызов
+// TODO пока не смотрел - надо разобраться
 
-Ну или просто (2-5)(5×7)
-*/
+/**
+ * Сглаживает токены, указанные в списке, и упрощает их.
+ */
+fun flatten(tokens: ArrayList<Any>, vararg ops: Operation) {
+    val tokenSwap = ArrayList<Any>()
+
+    tokenSwap.addAll(tokens)
+
+    while (!ops.none { tokens.contains(it) }) {
+        println(tokens.joinToString(separator = " "))
+
+        for (i in tokens.indices) {
+            var topBreak = false
+            for (o in ops) {
+                if (tokens[i] == o) {
+                    // левый и правый токен должен быть float
+                    if (i == 0 && o.requireLeftValue()) {
+                        throw IllegalArgumentException("Выражение не может начинаться с оператора: $o")
+                    } else if (i == tokens.size - 1 && o.requireRightValue()) {
+                        throw IllegalArgumentException("Выражение не может заканчиваться оператором: $o")
+                    }
+
+                    if (o.requireRightValue() && o.requireLeftValue()) {
+                        val a = tokens[i - 1] as? Float ?: throw IllegalArgumentException("Цепные операторы: $o и ${tokens[i - 1]}")
+                        val b = tokens[i + 1] as? Float ?: throw IllegalArgumentException("Цепные операторы: $o и ${tokens[i + 1]}")
+
+                        val v = o.execute(a, b)
+
+                        val localIndex = findIndex(tokens[i - 1], tokens[i], tokens[i + 1], list = tokenSwap)
+                        for (j in 0..2) {
+                            tokenSwap.removeAt(localIndex)
+                        }
+
+                        tokenSwap.add(localIndex, v)
+                        topBreak = true
+                        break
+                    } else if (o.requireRightValue()) {
+                        val b = tokens[i + 1] as? Float ?: throw IllegalArgumentException("Цепные операторы: $o и ${tokens[i + 1]}")
+
+                        val v = o.execute(0f, b)
+
+                        val localIndex = findIndex(tokens[i], tokens[i + 1], list = tokenSwap)
+                        for (j in 0..1) {
+                            tokenSwap.removeAt(localIndex)
+                        }
+
+                        tokenSwap.add(localIndex, v)
+                        topBreak = true
+                        break
+                    } else if (o.requireLeftValue()) {
+                        val a = tokens[i - 1] as? Float ?: throw IllegalArgumentException("Цепные операторы: $o и ${tokens[i - 1]}")
+
+                        val v = o.execute(a, 0f)
+
+                        val localIndex = findIndex(tokens[i - 1], tokens[i], list = tokenSwap)
+                        for (j in 0..1) {
+                            tokenSwap.removeAt(localIndex)
+                        }
+
+                        tokenSwap.add(localIndex, v)
+                        topBreak = true
+                        break
+                    }
+                }
+            }
+
+            if (topBreak)
+                break
+        }
+
+        tokens.clear()
+        tokens.addAll(tokenSwap)
+    }
+}
+
+/**
+ * находит ииндекс в заданном списке ArrayList
+ */
+fun findIndex(vararg objs: Any, list: ArrayList<Any>) =
+    list.indices.firstOrNull { i -> list[i] == objs[0] && objs.indices.none { list[i + it] != objs[it] } } ?: -1
