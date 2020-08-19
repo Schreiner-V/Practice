d8f6199884560be7d207e6a6a4339c1a655a480f
dimulka <dorofeev.dmitriy.gennadich@gmail.com>
2020-08-19 20:57:08 +0300

    Started UI

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,44 +1,90 @@
+import javafx.geometry.Orientation
+import javafx.geometry.Side
+import javafx.scene.control.TabPane
 import tornadofx.*
 
 // TODO Считай самое главное - надо добавить тесты на все возможные случаи. Будет гораздо удобнее
 // Сейчам почему-то 1+1 выражение он поймёт, а (1+1) нет, говорит, что скобки расставлены не верно, хотя это не так
 
-class MyApp: App(MyView::class)
+class MyApp : App(MyView::class)
 
-class MyView: View() {
-    override val root = vbox {
-        button("Press me")
-        label("Waiting")
+class MyView : View() {
+    override val root =
+        tabpane {
+            tabClosingPolicy = TabPane.TabClosingPolicy.UNAVAILABLE
+            side = Side.BOTTOM
+
+            tab<Tab1>()
+            tab<Tab2>()
+            tab<Tab3>()
+        }
+}
+
+class Tab1 : Fragment("Решение") {
+    override val root = form {
+        fieldset("Решение", labelPosition = Orientation.VERTICAL) {
+            field("Введите функцию") {
+                textfield() {
+                    requestFocus()
+                }
+                button("ОК") {
+
+                }
+            }
+        }
+        fieldset("Ход выполнения") {
+            textarea {
+            }
+        }
     }
 }
 
-fun main(args: Array<String>) {
-//    launch<MyApp>(args)
-
-    println(
-        """Определение переменных !x = 3
-           |Определение функций !f(x) = x^2
-           |Поддерживаемые операторы: % ! ^ √ * / + - > < = & |
-           |Логические операторы буду считать любое число больше 1 - true, меньше - false
-           |Логические операторы возвращают 1 - true, 0 - false
-           |Обозначать отрицательные числа: _, а не -
-           |Чтобы остановить приложение введите exit""".trimMargin()
-    )
-
-    val env = ExpressionEnvironment()
-    println("Константы:")
-
-    env.parseAndPrint("!e = 3")
-    env.parseAndPrint("!p = 3.14")
-
-    while (true) {
-        println("Введите выражение:")
-        val exprRaw = readLine() ?: continue
-
-        if (exprRaw.equals("exit", ignoreCase = true)) break
-
-        env.parseAndPrint(exprRaw)
+class Tab2 : Fragment("Теория") {
+    override val root = form {
+        fieldset("Теория", labelPosition = Orientation.VERTICAL) {
+            textarea {
+            }
+        }
     }
+}
+
+class Tab3 : Fragment("Тест") {
+    override val root = form {
+        fieldset("Тест", labelPosition = Orientation.VERTICAL) {
+            textarea {
+            }
+        }
+    }
+}
+
+
+fun main(args: Array<String>) {
+    launch<MyApp>(args)
+
+//    println(
+//        """Определение переменных !x = 3
+//           |Определение функций !f(x) = x^2
+//           |Поддерживаемые операторы: % ! ^ √ * / + - > < = & |
+//           |Логические операторы буду считать любое число больше 1 - true, меньше - false
+//           |Логические операторы возвращают 1 - true, 0 - false
+//           |Обозначать отрицательные числа: _, а не -
+//           |Чтобы остановить приложение введите exit""".trimMargin()
+//    )
+//
+//    val env = ExpressionEnvironment()
+//    println("Константы:")
+//
+//    env.parseAndPrint("!e = 3")
+//    env.parseAndPrint("!p = 3.14")
+//
+//    while (true) {
+//        println("Введите выражение:")
+//        val exprRaw = readLine() ?: continue
+//
+//        if (exprRaw.equals("exit", ignoreCase = true)) break
+//
+//        env.parseAndPrint(exprRaw)
+//    }
 }
 
 // TODO пока не смотрел - надо разобраться
