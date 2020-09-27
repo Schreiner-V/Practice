2653d865d0ae1239d93b6c16f37a0cd94cfd4164
Vladislav Schreiner <schreiner.v@bk.ru>
2020-09-27 05:42:37 +0300

    all math metods

--- src/main/kotlin/utils.kt
+++ src/main/kotlin/utils.kt
@@ -1,9 +1,6 @@
 import org.mariuszgromada.math.mxparser.Expression
 import kotlin.math.abs
-<<<<<<< HEAD
 import kotlin.math.min
-=======
->>>>>>> a2c8976ab101d1d5291bf1c93e00443c9adff276
 
 /* Как работают функции типа String. или любой другой объект Int.
 Допустим, у тебя есть какой-то объект из "чужого" API, методы которого ты не сможешь изменить.
@@ -28,14 +25,10 @@ fun String.calculate(): Double = Expression(this).calculate()
  */
 fun der(expression: String, argument: Double) = "der($expression, x, $argument)".calculate()
 
-<<<<<<< HEAD
 //TODO добавить расчетное время работы функций, графики, выгрузить теорию и тесты
 //TODO разобраться в каких случаях работает итерация, она работает с примером с сайта
 //TODO попробовать выводить случаи если корень посчитан точно, оформить выводы методов
 //TODO тесты, разобраться с тестами
-=======
-//TODO функция второй производной, добавить расчетное время работы функции, доделать итарацию,убрать дичь на 50-й строке
->>>>>>> a2c8976ab101d1d5291bf1c93e00443c9adff276
 
 /**
  * Вторая производная. Функция dern работает с ошибкой, поэтому используем der(der()).
@@ -45,43 +38,27 @@ fun der(expression: String, argument: Double) = "der($expression, x, $argument)"
  */
 fun secDer(expression: String, argument: Double) = "der(der($expression, x, x), x, $argument)".calculate()
 
-<<<<<<< HEAD
 //fun SecDer(expression: String, argument: Double) = "der($expression,x,$argument + 0.000001) - "
-=======
->>>>>>> a2c8976ab101d1d5291bf1c93e00443c9adff276
 //получаем x-3x
 // расширение класса  без наследования, https://tproger.ru/articles/kotlin-magic-functions/
 fun String.replaceAndCount(value: Double): Double = replace("x", "$value").calculate()
 
-<<<<<<< HEAD
 //
-=======
-// TODO сделать вывод первых трех уточнений корня, погрешности, итерации,если корень посчитан точно, то сообщить и т.д.
->>>>>>> a2c8976ab101d1d5291bf1c93e00443c9adff276
 
 fun bisection(left: Double, right: Double, accuracy: Double, function: String): Double {
     var newLeftEdge: Double = left
     var newRightEdge: Double = right
     var result: Double = newLeftEdge
-<<<<<<< HEAD
     var iter = 0
 
     while (newRightEdge - newLeftEdge >= accuracy) {
         iter++
         result = (newLeftEdge + newRightEdge) / 2.0
-=======
-
-    while (newRightEdge - newLeftEdge >= accuracy) {
-        result = (newLeftEdge + newRightEdge) / 2
->>>>>>> a2c8976ab101d1d5291bf1c93e00443c9adff276
 
         val fResult = function.replaceAndCount(result)
         val fLeft = function.replaceAndCount(newLeftEdge)
 
-<<<<<<< HEAD
 
-=======
->>>>>>> a2c8976ab101d1d5291bf1c93e00443c9adff276
         if (fResult == 0.0) {
             break
         }
@@ -90,21 +67,13 @@ fun bisection(left: Double, right: Double, accuracy: Double, function: String): 
         } else {
             newLeftEdge = result
         }
-<<<<<<< HEAD
         println("$iter . left - $newLeftEdge | right - $newRightEdge | x - $result")
     }
 
-=======
-    }
-
-    println(secDer(function, 1.0))
-
->>>>>>> a2c8976ab101d1d5291bf1c93e00443c9adff276
     return result
 }
 
 fun combination(left: Double, right: Double, accuracy: Double, function: String): Double {
-<<<<<<< HEAD
     //http://cyclowiki.org/wiki/%D0%9A%D0%BE%D0%BC%D0%B1%D0%B8%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%B9_%D0%BC%D0%B5%D1%82%D0%BE%D0%B4
     //
     fun startTimer() {}
@@ -155,24 +124,3 @@ do {
 
     return x1
 }
-=======
-
-    //http://cyclowiki.org/wiki/%D0%9A%D0%BE%D0%BC%D0%B1%D0%B8%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%B9_%D0%BC%D0%B5%D1%82%D0%BE%D0%B4
-    //
-
-    var a: Double = left //левый край
-    var b: Double = right // правый край
-    val fA = function.replaceAndCount(left) //функция от a
-    val fB = function.replaceAndCount(right) // функция от b
-
-    while (abs(a - b) > 2 * accuracy) {
-        when {
-            fA * secDer(function, a) < 0 -> a -= (fA * (b - a)) / (fB - fA)
-            fA * secDer(function, a) > 0 -> a -= fA / der(function, a)
-            fB * secDer(function, b) < 0 -> b -= fB * (b - a) / (fB - fA)
-            fB * secDer(function, b) > 0 -> b -= fB / der(function, b)
-        }
-    }
-    return (a + b) / 2.0
-}
->>>>>>> a2c8976ab101d1d5291bf1c93e00443c9adff276
