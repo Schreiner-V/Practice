0c6f7cc7d38b4ff208242e924bac71d2d2ff8026
dimulka <dorofeev.dmitriy.gennadich@gmail.com>
2020-10-06 00:01:02 +0300

    Graph added

--- src/main/kotlin/ui/SolutionTab.kt
+++ src/main/kotlin/ui/SolutionTab.kt
@@ -10,13 +10,18 @@ import combination
 import iteration
 import javafx.beans.property.SimpleObjectProperty
 import javafx.beans.property.SimpleStringProperty
+import javafx.collections.FXCollections
 import javafx.geometry.Orientation
 import javafx.scene.chart.NumberAxis
+import javafx.scene.chart.XYChart
 import javafx.scene.control.TextArea
 import replaceAndCount
 import tornadofx.*
+import kotlin.math.abs
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTimedValue
+
+private const val POINTS_COUNT_IN_GRAPH = 50
 
 class SolutionTab : Fragment("Решение") {
 
@@ -25,8 +30,9 @@ class SolutionTab : Fragment("Решение") {
     private val rightBorder = SimpleStringProperty()
     private val accuracy = SimpleStringProperty()
     private var logsTextArea: TextArea by singleAssign()
-
     private val selectedMethod = SimpleObjectProperty<Method>()
+
+    private val chartPoints = FXCollections.observableArrayList<XYChart.Data<Number, Number>>()
 
     @ExperimentalTime
     override val root = form {
@@ -67,13 +73,20 @@ class SolutionTab : Fragment("Решение") {
             }
             field {
                 button("Рассчитать") {
+                    disableProperty()
+                        .bind(inputString.isEmpty
+                        .or(leftBorder.isEmpty)
+                        .or(rightBorder.isEmpty)
+                        .or(accuracy.isEmpty))
                     action {
-                        this.isDisable = true
+                        val left = leftBorder.value.toDouble()
+                        val right = rightBorder.value.toDouble()
+
                         runAsync {
                             logsTextArea.appendText("-----------------------\nРасчёт начался...\n")
 
                             val timedValue = measureTimedValue {
-                                handleRun(inputString.value, leftBorder.value, rightBorder.value, accuracy.value, selectedMethod.value)
+                                handleRun(inputString.value, left, right, accuracy.value.toDouble(), selectedMethod.value)
                             }
 
                             logsTextArea.appendText(
@@ -83,23 +96,18 @@ class SolutionTab : Fragment("Решение") {
                                    |""".trimMargin()
                             )
                         } ui {
-                            this.isDisable = false
+                            chartPoints.clear()
+                            chartPoints.addAll(calculateGraphPoints(left, right, inputString.value))
                         }
                     }
                 }
             }
+            field {
+                linechart("График поведения функции", NumberAxis(), NumberAxis()) {
+                    series("График f(x)", chartPoints) {}
+                }
+            }
         }
-
-//        val arrayBis = graphic(-10.0, 10.0, inpStrMod)
-
-//        linechart("График поведения функции", NumberAxis(), NumberAxis()) {
-//            series("График f(x)") {
-//                for (i in 1..100){
-//                    println("[${arrayBis[i-1][0]}]-[${arrayBis[i-1][1]}]")
-//                    data(arrayBis[i-1][0],arrayBis[i-1][1])
-//                }
-//            }
-//        }
 
         fieldset("Ход выполнения") {
             textarea() {
@@ -114,19 +122,15 @@ class SolutionTab : Fragment("Решение") {
 
 private fun handleRun(
     inputString: String,
-    leftBorder: String,
-    rightBorder: String,
-    accuracyString: String,
+    leftBorder: Double,
+    rightBorder: Double,
+    accuracy: Double,
     method: Method
 ): Double {
-    val left = leftBorder.toDouble()
-    val right = rightBorder.toDouble()
-    val accuracy = accuracyString.toDouble()
-
     val result = when (method) {
-        BISECTION -> bisection(left, right, accuracy, inputString)
-        ITERATION -> iteration(left, right, accuracy, inputString)
-        COMBO -> combination(left, right, accuracy, inputString)
+        BISECTION -> bisection(leftBorder, rightBorder, accuracy, inputString)
+        ITERATION -> iteration(leftBorder, rightBorder, accuracy, inputString)
+        COMBO -> combination(leftBorder, rightBorder, accuracy, inputString)
     }.toString()
 
     return inputString
@@ -134,15 +138,12 @@ private fun handleRun(
         .calculate()
 }
 
-fun graphic(left: Double, right: Double, function: String): Array<DoubleArray> {
-    val frequency = (right - left) / 100.0
-    var newleft = left
-    val points = Array(100) { DoubleArray(2) }
+private fun calculateGraphPoints(left: Double, right: Double, function: String): List<XYChart.Data<Number, Number>> {
+    val piece = (abs(left) + abs(right)) / POINTS_COUNT_IN_GRAPH
 
-    for (i in 1..100) {
-        newleft += frequency
-        points[i - 1][0] = (newleft) // x
-        points[i - 1][1] = function.replaceAndCount((newleft)) // y
-    }
-    return points
+    return (0..POINTS_COUNT_IN_GRAPH)
+        .map {
+            val x = left + piece * it
+            XYChart.Data(x, function.replaceAndCount(x))
+        }
 }
